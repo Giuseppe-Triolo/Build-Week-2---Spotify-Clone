014f524eb390e1ec9d1a5ad07719882dffa9cf7c
Giuseppe-Triolo <giuseppe.triolo99@gmail.com>
2023-11-16 09:04:20 +0100

    Logica dinamica pagina artist

--- javascript/artist.js
+++ javascript/artist.js
@@ -18,6 +18,53 @@ async function getArtist() {
   return data;
 }
 
+async function getTopTracks(limit) {
+  const url = new URL(window.location.href);
+  const params = url.searchParams.get("id");
+  const apiUrl = `https://striveschool-api.herokuapp.com/api/deezer/artist/${params}/top?limit=${limit}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
+}
+
+function renderTopTracks(topTracks) {
+  if (topTracks.data) {
+    const tracksContainer = document.getElementById("tracksContainer");
+    topTracks.data.forEach((track) => {
+      const card = document.createElement("div");
+      card.className = "card bg-transparent text-white border-0 mx-0";
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+      const row = document.createElement("div");
+      row.className = "row";
+      const imgColumn = document.createElement("div");
+      imgColumn.className = "col-md-4";
+
+      if (track.album.cover_small) {
+        const img = document.createElement("img");
+        img.src = track.album.cover_small;
+        img.alt = track.title;
+        img.className = "img-fluid";
+        imgColumn.appendChild(img);
+      }
+
+      const textColumn = document.createElement("div");
+      textColumn.className = "col-md-8";
+      const title = document.createElement("h5");
+      title.className = "card-title";
+      title.textContent = track.title;
+      textColumn.appendChild(title);
+      row.appendChild(imgColumn);
+      row.appendChild(textColumn);
+      cardBody.appendChild(row);
+      card.appendChild(cardBody);
+      tracksContainer.appendChild(card);
+    });
+  } else {
+    console.error("Nessuna traccia trovata nell'album.");
+  }
+}
+
 const optionsArtist = {
   method: "GET",
   headers: {
@@ -50,7 +97,7 @@ const optionsDiscography = {
   }
 };
 
-const albumsPerPage = 5;
+let albumsPerPage = 5;
 let currentPage = 1;
 let totalAlbums;
 const displayedAlbums = new Set();
@@ -61,7 +108,6 @@ async function getArtistDiscography(artist) {
     const response = await fetch(urlDiscography, optionsDiscography);
     const data = await response.json();
     const canzoni = data.data.filter((canzone) => canzone.artist.id === artist.id);
-    const totalAlbums = canzoni.length;
     console.log(canzoni);
     return canzoni;
   } catch (asdasd) {
@@ -70,18 +116,32 @@ async function getArtistDiscography(artist) {
 }
 
 function showMore(canzoni) {
+  albumsPerPage = 1000;
   currentPage++;
   mostraAlbum(canzoni);
+  document.getElementById("showMoreButton").style.display = "none";
+}
+
+function addToSet(name, album) {
+  const nameAlreadyExists = Array.from(displayedAlbums).some((item) => item.album.title === name);
+  if (!nameAlreadyExists) {
+    displayedAlbums.add(album);
+  }
 }
 
 function mostraAlbum(canzoni) {
-  const start = (currentPage - 1) * albumsPerPage;
+  const start = currentPage === 1 ? 0 : 5;
   const end = start + albumsPerPage;
   const discographyContainer = document.getElementById("discography");
-  canzoni.slice(start, end).forEach((result) => {
-    const album = result.album;
-
-    if (!displayedAlbums.has(album.id)) {
+  const checkTest = new Set();
+  canzoni.forEach((x) => {
+    addToSet(x.album.title, x);
+  });
+  console.log(displayedAlbums);
+  Array.from(displayedAlbums)
+    .slice(start, end)
+    .forEach((result) => {
+      const album = result.album;
       const col = document.createElement("div");
       col.className = "col-md-2 mb-4";
       const card = document.createElement("div");
@@ -100,10 +160,7 @@ function mostraAlbum(canzoni) {
       card.appendChild(cardBody);
       col.appendChild(card);
       discographyContainer.appendChild(col);
-      displayedAlbums.add(album.id);
-    }
-  });
-
+    });
   if (end >= totalAlbums) {
     showMoreButton.style.display = "none";
   }
@@ -114,7 +171,8 @@ async function renderPage() {
   setBackground(artist);
   getArtistDiscography(artist);
   const canzoni = await getArtistDiscography(artist);
-
+  const topTracks = await getTopTracks(10);
+  renderTopTracks(topTracks);
   mostraAlbum(canzoni);
   const showMoreButton = document.getElementById("showMoreButton");
   showMoreButton.onclick = () => {
