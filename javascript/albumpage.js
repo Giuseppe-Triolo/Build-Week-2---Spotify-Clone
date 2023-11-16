e91dfe819e902febb86d002aa072947596ab73ca
xDerrick23 <dinodidiodoro23@gmail.com>
2023-11-16 17:24:21 +0100

    fetch

--- javascript/albumpage.js
+++ javascript/albumpage.js
@@ -179,9 +179,55 @@ function searchDeezer(query) {
   }
 
 // album fetch
-
-
-// artist fetch
+async function accessCollection(event) {
+    event.preventDefault();
+    const card = event.currentTarget;
+    const cardIdUrl = card.querySelector(".d-none");
+    const collectionUrl = cardIdUrl.innerText;
+    console.log("collectionUrl:", collectionUrl);
+    try {
+      const response = await fetch(collectionUrl, options);
+  
+      if (!response.ok) {
+        throw new Error("🔴 Network error");
+      }
+  
+      const output = await response.json();
+  
+      const innerHomeSection = document.getElementById("inner-home-section");
+      innerHomeSection.innerHTML = "";
+  
+      const section = document.createElement("section");
+      section.className = "mb-1 mb-sm-2 mb-md-3 mb-lg-4 mt-3";
+      innerHomeSection.appendChild(section);
+  
+      const elementContainer = document.createElement("div");
+      elementContainer.className = "d-flex gap-3";
+      section.appendChild(elementContainer);
+  
+      const imgContainer = document.createElement("div");
+      imgContainer.className =
+        "rounded-2 flex-shrink-0 lg-cover-container d-flex align-items-center me-3";
+      elementContainer.appendChild(imgContainer);
+  
+      if (output.picture) {
+        const albumCover = document.createElement("img");
+        albumCover.src = output.picture;
+  
+        if (output.type === "artist") {
+          albumCover.className = "img-fluid w-100 rounded-circle";
+        } else {
+          albumCover.className = "img-fluid w-100 rounded-1";
+        }
+  
+        imgContainer.appendChild(albumCover);
+      } else {
+        imgContainer.style.display = "none";
+      }
+    } catch (error) {
+      console.error("🔴 Fetching error:", error);
+    }
+  }
 
 // populate player
 async function populatePlayer(id, trackId){
@@ -245,24 +291,4 @@ async function pauseMusic(){
             style="background-color: transparent">  
                 <i style="font-size: 38px" class="bi bi-play-circle"></i>
             </button>`;
-}
-
-
-window.onload = () => {
-
-    loadFriends();
-
-    populatePlaylistColumn();
-
-    const param = new URLSearchParams(location.search)
-    const id = param.get("id");
-
-    popolaTracce(id);
-
-    const data = JSON.parse(localStorage.getItem('data'));
-    
-    if(localStorage.getItem('data')){
-        populatePlayer(...data);
-    }
-
-}
+}    
